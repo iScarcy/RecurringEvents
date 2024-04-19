d3e76be9cde9b38b637226a81a003f566b676ad5
iScarcy <56608800+iScarcy@users.noreply.github.com>
2024-04-19 10:17:28 +0200

    Ho aggiunto un nuovo controller per la ricerca degli eventi

---
+++ src/RecurringEvents.Web/Controllers/EventController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecurringEvents.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        /// <summary>
+        /// costruttore
+        /// </summary>
+        public EventController()
+        {
+
+        }
+        
+        /// <summary>
+        /// Restituisce tutti i compleanni 
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        [HttpGet("birdays")]
+        public Task<ActionResult> GetBirthdays() 
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Restituisce tutti gli onomastici
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("namedays")]
+        public Task<ActionResult> GetNameDays()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Restituisce tutti gli eventi
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        [HttpGet("all")]
+        public Task<ActionResult> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Restituisce tutti gli eventi di un determinato giorno
+        /// </summary>
+        /// <param name="day"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        [HttpGet("{day}")]
+        public Task<ActionResult> GetByDay(DateTime day)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Cerca tutti gli eventi legati ad una persona
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        [HttpGet("{person}")]
+        public Task<ActionResult> GetByPerson(string person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
