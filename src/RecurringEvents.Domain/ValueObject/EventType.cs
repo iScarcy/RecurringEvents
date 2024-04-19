7b245d42be016d9ebfca9a1500e21867dba5dd17
iScarcy <scarcella.giuseppe@gmail.com>
2024-04-20 00:34:30 +0200

    event type con annotazioni

--- src/RecurringEvents.Domain/ValueObject/EventType.cs
+++ src/RecurringEvents.Domain/ValueObject/EventType.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RecurringEvents.Domain.ValueObject
 {
-    public enum EventType
+    public enum EventType 
     {
+        [Display(Name = "Compleanno")]
         BirthDay,
+        [Display(Name = "Onomastico")]
         NameDay,
+        [Display(Name = "Anniversario")]
         Anniversary,
+        [Display(Name = "Promemoria")]
         Reminder
     }
 }
