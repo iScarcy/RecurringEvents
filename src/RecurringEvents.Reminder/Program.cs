49486e2518cf9904551e3d166f5d314b0364d5b2
iScarcy <56608800+iScarcy@users.noreply.github.com>
2024-04-24 09:46:30 +0200

    ho aggiunto un nuovo progetto che sostituirà .Remember

---
+++ src/RecurringEvents.Reminder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RecurringEvents.Reminder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
