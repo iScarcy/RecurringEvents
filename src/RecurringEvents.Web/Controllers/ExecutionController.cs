8c8da6c9d9312a575a6520da0c01ead59d5f259a
iScarcy <scarcella.giuseppe@gmail.com>
2024-04-25 01:48:45 +0200

    nuovo controller per le esecuzioni

---
+++ src/RecurringEvents.Web/Controllers/ExecutionController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecurringEvents.Web.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ExecutionController : ControllerBase
+{
+}
